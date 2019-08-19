36ca21e84a33d1257d7588ecb014893e8273f60f
Adolfo <adolfodelarosa2012@gmail.com>
2019-08-19 17:04:08 +0200

    Enviando una petición al REST API con datos concretos y obteniendo resultados

--- js/api.js
+++ js/api.js
@@ -19,4 +19,13 @@ class API {
             monedas
         };
     }
+
+    async obtenerValores( moneda, criptomoneda ){
+        const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}&api_key=${this.apikey}`;
+
+        // Consultar en el REST API
+        const urlConvertir = await fetch(url);
+        const resultado = await urlConvertir.json();
+        return { resultado };
+    }
 }
